1df3da3f28630f535d39b57912f270f622069df6
svetrie <vetsenthilkumar@gmail.com>
2018-02-25 16:23:57 -0600

    Added method to Time class that keeps track of time passed

--- src/com/example/Market.java
+++ src/com/example/Market.java
@@ -4,4 +4,31 @@ public class Market {
     private Food[] marketFoods;
     private Equipment[] marketEquipment;
     private Recipe[] marketRecipes;
+
+    public Food getFoodByName(String foodName) {
+        for (Food food : marketFoods) {
+            if (food.getName().equalsIgnoreCase(foodName)) {
+                return food;
+            }
+        }
+        return null;
+    }
+
+    public Equipment getEquipmentByName(String equipmentName) {
+        for (Equipment equipment : marketEquipment) {
+            if (equipment.getName().equalsIgnoreCase(equipmentName)) {
+                return equipment;
+            }
+        }
+        return null;
+    }
+
+    public Recipe getRecipeByName(String recipeName) {
+        for (Recipe recipe : marketRecipes) {
+            if (recipe.getCookedDish().getName().equalsIgnoreCase(recipeName)) {
+                return recipe;
+            }
+        }
+        return null;
+    }
 }
--- src/com/example/Time.java
+++ src/com/example/Time.java
@@ -1,6 +1,23 @@
 package com.example;
 
 public class Time {
-    private static int hour;
-    private static int min;
+    private static final int MIN_IN_HOUR = 60;
+    private static final int HOURS_IN_DAY = 24;
+    private static int hours;
+    private static int minutes;
+
+    public static void printTime() {
+        System.out.println(hours + ":" + minutes);
+    }
+
+    public static void passTime(int minPassed) {
+        if (minPassed > MIN_IN_HOUR) {
+            hours += minPassed / MIN_IN_HOUR;
+            minutes = minPassed % MIN_IN_HOUR;
+        }
+
+        if (hours >= HOURS_IN_DAY) {
+            hours = hours / HOURS_IN_DAY;
+        }
+    }
 }
