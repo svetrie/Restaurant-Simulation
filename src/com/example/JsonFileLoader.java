5f4089e2691f8d05939885d7ee73d1a0c76e3991
svetrie <vetsenthilkumar@gmail.com>
2018-02-27 01:42:58 -0600

    Created class to load market and restaurant from JSON files

---
+++ src/com/example/JsonFileLoader.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class JsonFileLoader {
+    
+    public static Restaurant getRestaurantFromJsonFile(String filename) {
+        Restaurant restaurant = null;
+        Gson gson = new Gson();
+
+        final Path path = FileSystems.getDefault().getPath("src/com/example/" + filename);
+
+        try {
+            String jsonString = new String(Files.readAllBytes(path));
+            restaurant = gson.fromJson(jsonString, Restaurant.class);
+        } catch (IOException e) {
+            System.out.println("Couldn't find file: " + filename);
+        }
+
+        return restaurant;
+    }
+
+    public static Market getMarketFromJsonFile(String filename) {
+        Market market = null;
+        Gson gson = new Gson();
+
+        final Path path = FileSystems.getDefault().getPath("src/com/example/" + filename);
+
+        try {
+            String jsonString = new String(Files.readAllBytes(path));
+            market = gson.fromJson(jsonString, Market.class);
+        } catch (IOException e) {
+            System.out.println("Couldn't find file: " + filename);
+        }
+
+        return market;
+    }
+
+}
